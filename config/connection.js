cc6192ac71c9890beb05ce166cf460df737e2032
Nihal-a <nihalavulan1@gamil.com>
2021-12-09 09:44:44 +0530

    Monogdb url updated

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db:null
 }
 module.exports.connect = function(done){
-    const url = 'mongodb://localhost:27017'
+    const url = `mongodb+srv://jobsway:${process.env.MONGODB_PASSWORD}@jobsway.beeub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
     const name = 'jobsWay'
 
     mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
