4709a2dec087c76bf8c8b75254951c48523c8ff7
Nihal-a <nihalavulan1@gamil.com>
2021-12-29 15:56:57 +0530

    apply job middleware changed

--- middlewares/JobMiddleware.js
+++ middlewares/JobMiddleware.js
@@ -2,13 +2,13 @@ var {check} = require('express-validator')
 
 module.exports = {
     validateApplyJob : [
-        check('formData.firstName','First name must be 3 characters long').exists().isLength({min : 3}),
-        check('formData.lastName','Last name cannot be blank').exists().isLength({min : 1}),
-        check('formData.email','Enter a valid email address').exists().isEmail(),
-        check('formData.phone','Enter a valid Phone number').exists().isLength({min : 10}),
-        check('formData.location','Enter a valid location').exists().isLength({min : 3}),
-        check('formData.experience','Enter a valid year of expirence').exists(),
-        check('formData.portfolio','Enter a valid portfolio URL').exists().isURL(),
+        check('firstName','First name must be 3 characters long').exists().isLength({min : 3}),
+        check('lastName','Last name cannot be blank').exists().isLength({min : 1}),
+        check('email','Enter a valid email address').exists().isEmail(),
+        check('phone','Enter a valid Phone number').exists().isLength({min : 10}),
+        check('location','Enter a valid location').exists().isLength({min : 3}),
+        check('experience','Enter a valid year of expirence').exists(),
+        check('portfolio','Enter a valid portfolio URL').exists().isURL(),
         check('image','You must upload your Photo').exists().isBase64(),
     ],
 }
